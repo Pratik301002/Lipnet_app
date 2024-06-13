d4b0b38271fc96f9f0c38e9a41839983ddfb1104
Pratik301002 <159586077+Pratik301002@users.noreply.github.com>
2024-06-14 02:06:04 +0530

    Files.upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import os
+from pathlib import Path
+from moviepy.editor import VideoFileClip
+import imageio
+import tensorflow as tf
+from utils import load_data, num_to_char
+from modelutil import load_model
+
+def convert_mpg_to_mp4(input_file, output_file):
+    try:
+        # Load the .mpg file
+        clip = VideoFileClip(input_file)
+
+        # Write the result to a .mp4 file
+        clip.write_videofile(output_file, codec='libx264')
+        return output_file
+
+    except Exception as e:
+        st.error(f"Error during conversion: {e}")
+        return None
+
+st.set_page_config(layout='wide')
+
+# Setup the sidebar
+with st.sidebar:
+    st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
+    st.title('LipBuddy')
+    st.info('This application is originally developed from the LipNet deep learning model.')
+
+st.title('LipNet Full Stack App')
+
+# Generating a list of options or videos
+options = os.listdir(os.path.join('data', 's1'))
+selected_video = st.selectbox("Choose video", options)
+col1, col2 = st.columns(2)
+
+if options:
+    # Rendering the video
+    with col1:
+        st.info('The video below displays the converted video in mp4 format')
+        file_path = os.path.join('data', 's1', selected_video)
+        output_path = os.path.join("converted_video.mp4")  # Use raw string
+        file_path2 = os.path.join('data', 's1', selected_video)
+        converted_file = convert_mpg_to_mp4(file_path, output_path)
+
+        if converted_file:
+            # Rendering inside of the app
+            video = open(output_path, 'rb')
+            video_bytes = video.read()
+            st.video(video_bytes)
+
+    with col2:
+        st.info('This is all the machine learning model sees when making a prediction')
+        video, annotations = load_data(tf.convert_to_tensor(file_path))
+        imageio.mimsave("animation.gif",video,fps=10)
+        st.image("animation.gif",width = 500)
+
+        st.info('This is the ouput of our machine learning model as tokens')
+
+        model = load_model()
+
+        y_hat = model.predict(tf.expand_dims(video,axis=0))
+        decoder = tf.keras.backend.ctc_decode(y_hat, [75], greedy=True)[0][0].numpy()
+        st.text(decoder)
+
+        st.info('Decode the raw tokens into words')
+        converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
+        st.text(converted_prediction)
---
+++ modelutil.py
@@ -0,0 +1,32 @@
+import os
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
+
+def load_model() -> Sequential:
+    model = Sequential()
+
+    model.add(Conv3D(128, 3, input_shape=(75,46,140,1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(Conv3D(256, 3, padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(Conv3D(75, 3, padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(TimeDistributed(Flatten()))
+
+    model.add(Bidirectional(LSTM(128, return_sequences=True)))
+    model.add(Dropout(.5))
+
+    model.add(Bidirectional(LSTM(128, return_sequences=True)))
+    model.add(Dropout(.5))
+
+    model.add(Dense(41, activation='softmax'))
+
+    model.load_weights("lipnet_model.h5")
+
+    return model
